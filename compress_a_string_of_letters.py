394f82a4267f07f93846a3ebf4c6f84119083797
LizzieDworsky <amydworsky15@gmail.com>
2022-05-10 09:45:38 -0600

    string compression started

--- compress_a_string_of_letters.py
+++ compress_a_string_of_letters.py
@@ -1,2 +1,40 @@
 #For example an input of "aaabbbbbccccaacccbbbaaabbbaaa"
 #would compress to "3a5b4c2a3c3b3a3b3a"
+
+#if previous index (or next one) is equal to then add to a concatination
+# then use len to add the number
+# not sure yet how to replicate the letter in ir also
+
+
+string_input_for_compression = input ("Please enter string for compression: ")
+
+number_in_string = len(string_input_for_compression)
+print (number_in_string)
+
+
+first_concat = ""
+first_letter = ""
+location = ""
+for index in range (0, len (string_input_for_compression), 1):
+    first_letter = string_input_for_compression [0]
+    if string_input_for_compression [index] == first_letter:
+        first_concat += string_input_for_compression [index]
+    else:
+        location = index
+        break
+number_in_first_concat = len (first_concat)
+print (f"{number_in_first_concat}{first_letter}")
+
+second_concat = ""
+for index in range (location, len (string_input_for_compression), 1):
+    first_letter = string_input_for_compression [location]
+    if string_input_for_compression [index] == first_letter:
+        second_concat += string_input_for_compression [index]
+    else:
+        location = index
+        break
+number_in_second_concat = len (second_concat)
+print (f"{number_in_second_concat}{first_letter}")
+
+
+#now I need to create a loop that runs through this code indefintely until there are no more characters in the string.
