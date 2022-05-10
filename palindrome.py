85b6991b9b633b0825323862c298ee1f8862672c
LizzieDworsky <amydworsky15@gmail.com>
2022-05-10 08:43:25 -0600

    palindrome function updated

--- palindrome.py
+++ palindrome.py
@@ -2,11 +2,12 @@
 #"madam"
 #Write code that takes a user input and checks to see if it is a Palindrome and reports the result
 
-user_input_palindrome_checker = input ("Please enter a word or phrase. We will check if it is written the same forward and backward. ")
-reversed_input_palindrome_checker = ""
-for index in range (len(user_input_palindrome_checker) -1, -1, -1):
-    reversed_input_palindrome_checker += user_input_palindrome_checker [index]
-if user_input_palindrome_checker == reversed_input_palindrome_checker:
-    print ("Congrats! This is a Palindrome!")
-else:
-    print ("This is not a Palindrome")
+def palindrome_checker ():
+    user_input_palindrome_checker = input ("Please enter a word or phrase. We will check if it is written the same forward and backward. ")
+    reversed_input_palindrome_checker = ""
+    for index in range (len(user_input_palindrome_checker) -1, -1, -1):
+        reversed_input_palindrome_checker += user_input_palindrome_checker [index]
+    if user_input_palindrome_checker == reversed_input_palindrome_checker:
+        print ("Congrats! This is a Palindrome!")
+    else:
+        print ("This is not a Palindrome")
