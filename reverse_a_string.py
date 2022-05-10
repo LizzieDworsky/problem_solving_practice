8df8f689869e7c29cf6b9dc0f9bda4e30dd56b87
LizzieDworsky <amydworsky15@gmail.com>
2022-05-10 08:27:35 -0600

    reverse string function updated

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -2,8 +2,9 @@
 # write code that takes a string as input and returns the string reversed
 #"Hello" will be returned as "olleH"
 
-user_input_word_or_phrase = input ("Please enter a word or phrase: ")
-reversed_input_word_or_phrase = ""
-for index in range (len(user_input_word_or_phrase) -1, -1, -1):
-    reversed_input_word_or_phrase += user_input_word_or_phrase [index]
-print (reversed_input_word_or_phrase)
+def reverse_user_input ():
+    user_input_word_or_phrase = input ("Please enter a word or phrase: ")
+    reversed_input_word_or_phrase = ""
+    for index in range (len(user_input_word_or_phrase) -1, -1, -1):
+        reversed_input_word_or_phrase += user_input_word_or_phrase [index]
+    print (reversed_input_word_or_phrase)
